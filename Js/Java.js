dddcaacfb960110e033db8fa1d64a6439e99244a
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-09-29 19:32:19 -0300

    teste final

--- Js/Java.js
+++ Js/Java.js
@@ -2,3 +2,29 @@ function clickProduto (produto,valor) {
 	alert(`${produto} custa R$ ${valor} e foi adicionado ao carrinho!`);
 
 }
+
+function validacao() {
+	var formulario = document.forms["formcadastro"]
+	var nome = formulario.name.value
+	var telefone = formulario.tel.value
+	var endereco = formulario.adress.value
+	
+
+	var erro = false
+
+	if (formulario.name.value=='') {
+		alert("Oops! Este campo precisa ser preenchido.")
+	}
+
+	if (telefone.length < 11) {
+		alert ("Oops! Parece que seu telefone não possui todos os dígitos.")
+		erro = true // quando identifica um erro 
+	}
+	 
+	if (erro) { // se houver algum erro, retorna falso
+		return false
+	} else {
+		return true
+	}
+
+}
